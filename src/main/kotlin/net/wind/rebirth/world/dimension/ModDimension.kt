f95086ccc380aaa0c78050bb0dbe7cd43827f078
Wind292 <jp3@jayps.net>
2023-10-18 20:58:22 -0400

    Added Nothing

---
+++ src/main/kotlin/net/wind/rebirth/world/dimension/ModDimension.kt
@@ -0,0 +1,21 @@
+//import net.fabricmc.api.ModInitializer
+//import net.fabricmc.fabric.api.dimension.v1.FabricDimensions
+//import net.minecraft.util.Identifier
+//import net.minecraft.world.dimension.DimensionType
+//
+//class MyMod : ModInitializer {
+//    override fun onInitialize() {
+//        MY_DIMENSION = FabricDimensions.registerDimension(MY_DIMENSION_ID, MY_DIMENSION, null, true)
+//    }
+//
+//
+//
+//    companion object {
+//        val MY_DIMENSION_ID = Identifier("mymod", "my_dimension")
+//        var MY_DIMENSION: DimensionType? = null
+//    }
+//
+//
+//
+//
+//}
